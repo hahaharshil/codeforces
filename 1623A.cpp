60874ef22b7a41fedfae77eb176c6997c48906c3
Harshil kumar <hahaharshil@pm.me>
2022-04-03 15:01:29 +0530

    3 April '22

---
+++ 1623A.cpp
@@ -0,0 +1,20 @@
+//https://codeforces.com/problemset/problem/1613/A
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    for(int i = 0; i < n; i++){
+        int a,b,c,d;cin >> a >> b >> c >> d;
+        
+        int power = a*(pow(10,b));
+        int power2 = c*(pow(10,d));
+        
+        if(power < power2) cout << "<" << endl;
+        else if(power > power2) cout << ">" << endl;
+        else cout << "=" << endl;
+    }
+    return 0;
+}
