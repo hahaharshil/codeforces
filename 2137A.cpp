facb7b95a09bc37023317653f2807a7ebce4d357
hahaharshil <hahaharshil@pm.me>
2025-09-08 18:33:04 +0530

    Codeforces Round 1047 (Div 3) Problem A

---
+++ 2137A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+//337368622 	Sep/07/2025 21:01UTC+5.5 	hahaharshilpp 	A - Collatz Conjecture 	C++20 (GCC 13-64) 	Accepted 	46 ms 	0 KB 
+
+
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    for(int i = 0; i < t;i++){
+        int k, n; cin >> k >> n;
+        for(int j = 0; j < k; j++){
+                n *= 2;
+            }
+            cout << n << endl;
+        }
+}
